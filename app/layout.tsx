62bc465515e6d299ea18711806da053d91c5bc94
Ankit Verma <av78751170@gmail.com>
2023-10-28 22:02:02 +0530

    updated some files

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,29 +1,62 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
-import StarsCanvas from '@/Components/main/StarBackground'
-import Navbar from '@/Components/main/Navbar'
+// import type { Metadata } from 'next'
+// import { Inter } from 'next/font/google'
+// import './globals.css'
+// import StarsCanvas from '@/Components/main/StarBackground'
+// import Navbar from '@/Components/main/Navbar'
 
-const inter = Inter({ subsets: ['latin'] })
+// const inter = Inter({ subsets: ['latin'] })
+
+// export const metadata: Metadata = {
+//   title:" Ankit's Portfolio" ,
+//   description: 'This is my Portfolio',
+// }
+
+// export default function RootLayout({
+//   children,
+// }: {
+//   children: React.ReactNode
+// }) {
+//   return (
+//     <html lang="en">
+//       <body className={`${inter.className}  bg-[#030014] overflow-y-scroll overflow-x-hidden`}>
+//       <StarsCanvas />
+//         <Navbar />
+        
+//         {children}
+//         </body>
+//     </html>
+//   )
+// }
+
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import StarsCanvas from "@/Components/main/StarBackground";
+import Navbar from "@/Components/main/Navbar";
+import Footer from "@/Components/main/Footer";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title:" Ankit's Portfolio" ,
-  description: 'This is my Portfolio',
-}
+  title: "Space Portfolio",
+  description: "This is my portfolio",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className}  bg-[#030014] overflow-y-scroll overflow-x-hidden`}>
-      <StarsCanvas />
+      <body
+        className={`${inter.className} bg-[#030014] overflow-y-scroll overflow-x-hidden`}
+      >
+        <StarsCanvas />
         <Navbar />
-        
         {children}
-        </body>
+        <Footer />
+      </body>
     </html>
-  )
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -5,17 +5,18 @@ import Projects from '@/Components/main/Projects'
 import Skills from '@/Components/main/Skills'
 import Image from 'next/image'
 
+
+
+
 export default function Home() {
   return (
-    <main className="h-full w-full" >
-      <div className='flex flex-col gap-20'>
+    <main className="h-full w-full">
+      <div className="flex flex-col gap-20">
         <Hero />
         <Skills />
         <Encryption />
         <Projects />
-        <Footer />
-
       </div>
     </main>
-  )
+  );
 }
